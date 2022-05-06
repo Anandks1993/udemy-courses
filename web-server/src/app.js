b7a50b4ae223bffc7b0823b1b3b4a0738622267f
Anand KS <anandks@cloudix.io>
2022-05-06 15:01:14 +0530

    web server more update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,21 @@
 const path = require('path');
 
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirPath = path.join(__dirname, '../public');
 
+// Define paths for express config.
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location.
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve.
 app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
@@ -24,7 +34,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.'
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Anand KS'
     })
 })
 
@@ -35,9 +47,25 @@ app.get('/weather', (req, res) => {
     });
 });
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'Help article not found.',
+        title: '404',
+        name: 'Anand KS'
+    })
+});
+
 // app.com
 // app.com/help
 // app.com/about
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'Page not found.',
+        title: '404',
+        name: 'Anand KS'
+    });
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
