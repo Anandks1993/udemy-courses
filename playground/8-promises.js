1802b3c2c9ff0148b7b7199b36677d1638f4d343
Anand KS <anandks@cloudix.io>
2022-05-27 11:50:25 +0530

    task manager create and read operation

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,42 @@
-const doWorkCallback = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject('This is my error!');
-        // resolve([1, 2, 3, 4]);
-    }, 2000);
-});
+// const doWorkCallback = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject('This is my error!');
+//         // resolve([1, 2, 3, 4]);
+//     }, 2000);
+// });
 
-doWorkCallback.then(res => {
-    console.log('Success!', res);
-}).catch(error => {
-    console.log('Error!', error);
-})
+// doWorkCallback.then(res => {
+//     console.log('Success!', res);
+// }).catch(error => {
+//     console.log('Error!', error);
+// })
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000);
+    });
+};
+
+// add(1, 2).then((sum) => {
+//     console.log(sum);
+
+//     add(sum, 3).then(sum2 => {
+//         console.log(sum2);
+//     }).catch(err => {
+//         console.log(err);
+//     })
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// PROMISE CHAINING.
+add(1, 2).then(sum => {
+    console.log(sum);
+    return add(sum, 3);
+}).then(sum2 => {
+    console.log(sum2);
+}).catch(err => {
+    console.log(err);
+});
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,58 +1,74 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+// const validator = require('validator');
 
+// MongoDB connection
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-});
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid');
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minLength: 7,
+//         validate(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('Password cannot contain "password"');
+//             }
+//         }
+//     }
+// });
 
-const me = new User({
-    name: '   Anand   ',
-    email: 'MYEMAIL@MEAD.IO    '
-});
+// const me = new User({
+//     name: '   Anand   ',
+//     email: 'MYEMAIL@MEAD.IO    ',
+//     password: 'phone098!'
+// });
 
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log('Error!', error);
-});
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) => {
+//     console.log('Error!', error);
+// });
 
-const Tasks = mongoose.model('Tasks', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
+// const Tasks = mongoose.model('Tasks', {
+//     description: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     }
+// });
 
 // const task = new Tasks({
-//     description: 'Buy TV',
+//     description: 'Buy Sofa Set',
 //     completed: false
 // });
 
